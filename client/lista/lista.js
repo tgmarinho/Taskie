f4472e02cd00245f2f4286a4a33fb17ceac7ac5d
Thiago Marinho <tgmarinho@gmail.com>
2017-07-06 18:16:10 -0400

    criando nova tarefa e inserindo na collection do mongo¨

--- client/lista/lista.js
+++ client/lista/lista.js
@@ -2,6 +2,10 @@ Template.lista.helpers({
 
     tarefas : function(){
         return Tarefas.find({});
+    },
+
+    formataData : function(){
+        return moment(this.data).format('DD/MM/YYYY HH:mm');
     }
 
 
---
+++ client/novo/novo.js
@@ -0,0 +1,15 @@
+Template.novo.events({
+
+  "submit #adicionaTarefa" : function(e, template){
+    e.preventDefault();
+
+    var input = $("#tarefa");
+    var nome = input.val();
+
+    Tarefas.insert({nome: nome, data: new Date()});
+    input.val("");
+    intput.focus();
+
+  }
+
+});
