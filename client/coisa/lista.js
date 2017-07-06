14775ae19e828352157adaad72e20ca1993246da
Thiago Marinho <tgmarinho@gmail.com>
2017-07-06 14:28:08 -0400

    projeto taskie inicial commit

---
+++ client/coisa/lista.js
@@ -0,0 +1,13 @@
+Template.coisa.helpers({
+
+    tarefas : function(){
+        return [
+            {nome: "Ir ao mercado"},
+            {nome: "comprar bala"},
+            {nome: "estudar"}
+        ];
+
+    }
+
+
+});
